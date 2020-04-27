0c4034b897f5f25a13b324da9948b6b0e276393d
rajathapa <rajat@bizcover.com.au>
2020-04-27 13:09:05 +1000

    nz textbox

--- app.js
+++ app.js
@@ -3,34 +3,55 @@ var app = angular.module('myApp', ['ui.mask']);
 app.controller('MainCtrl', ['$scope', function ($scope) {
   var vm = this;
   vm.name = 'Ui Mask';
-  vm.phone = '';
+  vm.phoneAU = '';
+  vm.phoneNZ = '';
   vm.defaultPlaceholder = '04xx xxx xxx';
+  vm.defaultPlaceholderNZ = '02x xxx xxxxx';
+
   vm.regExpPhone = '^[1,0]\\d{9}$';
-  vm.messageErrorPhone = 'Please enter your phone number starting with a 0 or 1';
-// mimic refresh
-  if (localStorage.phone != '') {
-    vm.phone = localStorage.phone;
-  }
+  vm.regExpPhoneNz = '^[0]\\d{8}$|^[0][2][1,2,7,0]\\d{6,8}$'
+  vm.messageErrorPhoneAU = 'Please enter your phone number starting with a 0 or 1 AU.';
+  vm.messageErrorPhoneNZ = 'Please enter a valid phone number NZ.';
+  // mimic refresh
+
+  vm.phoneAU = localStorage.phoneAU;
+  vm.phoneNZ = localStorage.phoneNZ;
 
 
   vm.updatePhone = function () {
-    if (!vm.isValidPhone()) {
+    if (!vm.isValidPhone(vm.regExpPhone, vm.phoneAU)) {
       return;
     }
     // save in localstorage for retrival
-    localStorage.phone = vm.phone;
+    localStorage.phoneAU = vm.phoneAU;
   };
 
-
-  vm.isValidPhone = function () {
-    if(vm.phone === null || angular.isUndefined(vm.phone) || vm.phone === ''){
-            return false;
+  vm.updatePhoneNZ = function () {
+    if (!vm.isValidPhone(vm.regExpPhoneNz, vm.phoneNZ)) {
+      return;
     }
-    var phone = vm.phone;
+    // save in localstorage for retrival
+    localStorage.phoneNZ = vm.phoneNZ;
+  };
+
+  vm.isValidPhoneNZ = function () {
+    return vm.isValidPhone(vm.regExpPhoneNz, vm.phoneNZ);
+  }
+
+  vm.isValidPhoneAU = function () {
+    return vm.isValidPhone(vm.regExpPhone, vm.phoneAU);
+  }
+
+  vm.isValidPhone = function (reg, phone) {
+    if (phone === null || angular.isUndefined(phone) || phone === '') {
+      return false;
+    }
+
     phone = phone.replace(/[()\-# ]/gi, '');
 
-    var re = new RegExp(vm.regExpPhone);
-    var result = re.test(phone);
+    var re = new RegExp(reg);
+    var result;
+    result = re.test(phone);
     return result;
   };
 }]);
@@ -39,24 +60,25 @@ app.directive('customPlaceholder', [function () {
   return {
     scope: {
       phoneNumber: "=",
-      pholder: "="
+      pholder: "=",
+      isAu: "@"
     },
     link: function (scope, element, attr) {
-      var landlineMask = '(99) 9999 9999';
-      var mobileMask = '9999 999 999';
+      var landlineMask = scope.isAu === "true" ? '(99) 9999 9999' : '(99) 999 9999';
+      var mobileMask = scope.isAu === "true" ? '9999 999 999' : '999 999 999?9?9';
+      var defaultMask = scope.isAu === "true" ? "9999999999" : '999999999?9?9';
+      var regExpMobilePrefix = scope.isAu === "true" ? '(^[1][38]00)|(^04)' : '^[0][2][1,2,7,0][\\d]*$';
 
-      var defaultMask = "9999999999";
-
-      var regExpMobilePrefix = '(^[1][38]00)|(^04)';
       var re = new RegExp(regExpMobilePrefix);
 
       angular.element(document).ready(function () {
-        if(scope.phoneNumber){
+        if (scope.phoneNumber) {
           maskPhoneNumber();
         }
       });
 
       element.on('focus', function () {
+        console.log(scope.isAu);
         if (!scope.phoneNumber) {
           attr.$set('uiMask', defaultMask);
           setPlaceholder(attr, defaultMask);
@@ -64,10 +86,9 @@ app.directive('customPlaceholder', [function () {
       });
 
       element.on('keyup', function () {
-        if (element.val().replace(" ", '').trim() === "( )" || 
+        if (element.val().replace(" ", '').trim() === "( )" ||
           element.val().replace(" ", '').trim() === ""
-        )
-        {
+        ) {
           attr.$set('uiMask', defaultMask);
           setPlaceholder(attr, defaultMask);
         }
@@ -95,10 +116,12 @@ app.directive('customPlaceholder', [function () {
         }
       }
 
-      function setPlaceholder(attr, value){
-         var replacePattern = /[a-zA-Z0-9]/g;
-         attr.$set('placeholder', value.replace(replacePattern, ' '));
+      function setPlaceholder(attr, value) {
+        var replacePattern = /[a-zA-Z0-9?]/g;
+        value = value.replace(replacePattern, ' ');
+        attr.$set('placeholder', value);
       }
     }
   }
 }]);
+
